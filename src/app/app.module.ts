f2ae754df6b6f6b464038fc836c76e5ac11c7c74
JD <jdandrews357@gmail.com>
2019-12-11 09:17:37 -0500

    minor fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-
 import { ScoresComponent } from "./components/scores/scores.component";
 import { GameComponent } from "./components/game/game.component";
 import { SplashComponent } from "./components/splash/splash.component";
